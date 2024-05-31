3a057f469c1e91e3cdac12a674b93a41a910c352
Atharva Bhoite <105184536+78033@users.noreply.github.com>
2024-05-31 23:20:11 +0530

    Add files via upload

---
+++ Pattern Printing Problems/54Pattern14.c
@@ -0,0 +1,27 @@
+// Input: 5
+// Output: A    B   C   D   E
+
+#include<stdio.h>
+
+void Display(int iNo){
+    int iCnt = 0;
+    char ch = '\0';
+
+    for(iCnt = 1,ch = 'A'; iCnt <= iNo; iCnt++, ch++){
+        printf("%c\t",ch);
+    }
+    printf("\n");
+}
+
+
+int main(){
+
+    int iValue = 0;
+    
+    printf("Enter number: \n");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}
