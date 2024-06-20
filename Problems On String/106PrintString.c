d7cdb57ceadcd6c8e0234b5d0098b3bdac04e25e
Atharva Bhoite <105184536+78033@users.noreply.github.com>
2024-06-20 12:00:36 +0530

    Add files via upload

---
+++ Problems On String/106PrintString.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    char arr[] = "Hello";
+
+    printf("%s\n",arr);
+
+    printf("%c\n",arr[0]);
+    printf("%c\n",arr[1]);
+    printf("%c\n",arr[2]);
+    printf("%c\n",arr[3]);
+    printf("%c\n",arr[4]);
+
+    return 0;
+}
