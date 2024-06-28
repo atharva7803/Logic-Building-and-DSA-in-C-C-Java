f1f0fd45d6268c31c709b800bb80524507f2594a
Atharva Bhoite <105184536+78033@users.noreply.github.com>
2024-06-28 23:38:43 +0530

    Add files via upload

---
+++ Problems On Recursion/Program178.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+// Approach 1 : Using for loop
+void Display(){
+    for(int i = 0; i < 4; i++){
+        cout<<"*"<<endl;
+    }
+}
+
+int main(){
+
+    Display();
+
+    return 0;
+}
