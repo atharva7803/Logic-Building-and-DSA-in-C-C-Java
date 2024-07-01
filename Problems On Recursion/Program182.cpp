5e782768be1a11fb819c3f49b86ce064f084ea5f
Atharva Bhoite <105184536+78033@users.noreply.github.com>
2024-07-01 13:54:24 +0530

    Add files via upload

---
+++ Problems On Recursion/Program182.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+// Approach 3 : Using Recursion
+void Display(int iNo){
+    static int i = 0;            // this statement executes only once 
+    if(i < iNo){
+        cout <<"*"<<endl;
+        i++;
+        Display(iNo);
+    }
+}
+
+int main(){
+
+    int iValue = 0;
+
+    cout<<"Enter number: "<<endl;
+    cin>>iValue;
+
+    Display(iValue);
+
+    cout<<"End of application!"<<endl;
+
+    return 0;
+}
