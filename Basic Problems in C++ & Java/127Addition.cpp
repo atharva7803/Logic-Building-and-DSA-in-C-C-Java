968f5a1d0f49a306ab62f5ca5b9efd6269e209f2
Atharva Bhoite <105184536+78033@users.noreply.github.com>
2024-07-07 12:31:40 +0530

    Add files via upload

---
+++ Basic Problems in C++ & Java/127Addition.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+// Approach 1
+
+int main(){
+    int iValue1 = 0, iValue2 = 0;
+    int iAns = 0;
+
+    cout<<"Enter first no: "<<endl;
+    cin>>iValue1;
+
+    cout<<"Enter second no: "<<endl;
+    cin>>iValue2;
+
+    iAns = iValue1 + iValue2;
+
+    cout<<"Addition is: "<<iAns<<endl;
+
+    return 0;
+}
