90000cf70910f30bc1a2d5c48ae8f6eab4e2ff3c
Atharva Bhoite <105184536+78033@users.noreply.github.com>
2024-07-09 13:57:36 +0530

    Add files via upload

---
+++ Basic Problems in C++ & Java/141Program.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+class ArrayX{
+    private:        // changed to private
+        int *Arr;
+        int iSize;
+
+    public:
+        ArrayX(int no){
+            cout<<"Inside Constructor"<<endl;
+            iSize = no;
+            Arr = new int[iSize];
+        }
+
+        ~ArrayX(){
+            cout<<"Inside Destructor"<<endl;
+            delete []Arr;
+        }
+
+        void Accept(){
+            cout<<"Enter the elements: "<<endl;
+            for(int i = 0; i < iSize; i++){
+                cin>>Arr[i];
+            }
+        }
+
+        void Display(){
+            cout<<"Elements are: "<<endl;
+            for(int i = 0; i < iSize; i++){
+                cout<<Arr[i]<<endl;
+            }
+        }
+};
+
+int main(){
+    
+    int iLen = 0;
+
+    cout<<"Enter the no of elem that you want to store: "<<endl;
+    cin>>iLen;
+
+    ArrayX aobj(iLen);
+
+    aobj.Accept();
+    aobj.Display();
+
+    // aobj.Arr = NULL;     this will give error as its private characteristics
+    // aobj.iSize = 20;
+
+    return 0;
+}
