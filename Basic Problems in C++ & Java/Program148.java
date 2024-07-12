d1f430df72e6fc17ca09eb93c60f0cfe324fef3e
Atharva Bhoite <105184536+78033@users.noreply.github.com>
2024-07-12 23:30:58 +0530

    Add files via upload

---
+++ Basic Problems in C++ & Java/Program148.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Program148 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); 
+
+        System.out.println("Enter no of elements: ");
+        int iSize = sc.nextInt();
+
+        int arr[] = new int[iSize];
+
+        System.out.println("Size of array is: "+ arr.length);
+
+    }
+}
