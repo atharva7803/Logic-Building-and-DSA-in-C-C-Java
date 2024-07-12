0da7edbdd1f7aa7ecb22f545afde88dd080ed091
Atharva Bhoite <105184536+78033@users.noreply.github.com>
2024-07-12 23:30:02 +0530

    Add files via upload

---
+++ Basic Problems in C++ & Java/146Program.cpp
@@ -0,0 +1,68 @@
+// Accept n number from user and calculate sum of digits of each numbers individually
+
+#include<iostream>
+using namespace std;
+
+class ArrayX{
+    private:        // changed to private
+        int *Arr;
+        int iSize;
+
+    public:
+        ArrayX(int no){
+            cout<<"Inside Constructor"<<endl;
+            iSize = no;
+            Arr = new int[iSize];
+        }
+
+        ~ArrayX(){
+            cout<<"Inside Destructor"<<endl;
+            delete []Arr;
+        }
+
+        void Accept(){
+            cout<<"Enter the elements: "<<endl;
+            for(int i = 0; i < iSize; i++){
+                cin>>Arr[i];
+            }
+        }
+
+        int SumDigits(int iNo) {
+                int iSum = 0;
+                int iDigit = 0;
+
+                while(iNo != 0){
+                    iDigit = iNo %  10;
+                    iSum = iSum + iDigit;
+                    iNo = iNo / 10;
+                }
+                return iSum;
+        }
+
+        void DisplayDigitCount(){       // heavy approach
+            int iRet = 0;
+            for(int i = 0; i < iSize; i++){
+                iRet = SumDigits(Arr[i]);
+                cout<<"Sum of digits of "<<Arr[i]<<" is: "<<iRet;
+            }
+        }
+};
+
+int main(){
+    
+    int iLen = 0;
+
+    cout<<"Enter the no of elements that you want to store: "<<endl;
+    cin>>iLen;
+
+    ArrayX *aobj = new ArrayX(iLen);        // Dynamic object creation
+
+    aobj->Accept();
+
+    aobj->DisplayDigitCount();
+
+
+    delete aobj;
+
+    return 0;
+}
