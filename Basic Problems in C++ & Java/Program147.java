ab869fa5ebec22b3038c363b5e127e077ebc2340
Atharva Bhoite <105184536+78033@users.noreply.github.com>
2024-07-12 23:30:31 +0530

    Add files via upload

---
+++ Basic Problems in C++ & Java/Program147.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Program147 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); 
+        int arr[] = {10,20,30,40};
+
+        System.out.println("Number of elements in arr: "+ arr.length);
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
