825bd4dfc6e673fe0a7a7d8c4bcac4e22032eb04
Atharva Bhoite <105184536+78033@users.noreply.github.com>
2024-07-14 11:58:07 +0530

    Add files via upload

---
+++ Basic Problems in C++ & Java/Program153.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char arr[30];
+
+    cout<<"Enter your name: "<<endl;
+    cin>>arr;
+
+    cout<<"Hello "<<arr<<endl;
+    
+    return 0;
+
+}
