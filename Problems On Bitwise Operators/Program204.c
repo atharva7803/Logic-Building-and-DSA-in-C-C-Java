0916858eacb53e187872aab6b34ade7c24e8b9c5
Atharva Bhoite <105184536+78033@users.noreply.github.com>
2024-08-02 23:01:02 +0530

    Add files via upload

---
+++ Problems On Bitwise Operators/Program204.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+// Bitwise XOR (^)
+int main(){
+    unsigned int iNo1 = 0, iNo2 = 0, iRet = 0;
+
+    printf("Enter first number: \n");
+    scanf("%d", &iNo1);
+
+    printf("Enter second number: \n");
+    scanf("%d", &iNo2);
+
+    iRet = iNo1 ^ iNo2;
+
+    printf("Result of bitwise ^ is: %u\n",iRet);
+
+    return 0;
+}
+
+
+/*      
+        --------------------------------
+        OP1     OP2     &      |     ^
+        --------------------------------
+        1       1       1      1     0
+        1       0       0      1     1   
+        0       1       0      1     1      
+        0       0       0      0     0
+        -------------------------------
+        
+
+        iNo1        11      0 0 0 0 1 0 1 1
+        iNo1        15      0 0 0 0 1 1 1 1
+        ------------------------------------- &
+                            0 0 0 0 0 1 0 0   = 4
+
+
+        iNo1        67      0 1 0 0 0 0 1 1
+        iNo1        34      0 0 1 0 0 0 1 0
+        ------------------------------------- &
+                            0 1 1 0 0 0 0 1   = 97  
+*/
+
+
+
+
