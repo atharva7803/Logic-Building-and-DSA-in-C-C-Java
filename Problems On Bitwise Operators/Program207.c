fae2751cdee07f8ca2fca5ff9586b59e7246fe94
Atharva Bhoite <105184536+78033@users.noreply.github.com>
2024-08-03 19:22:54 +0530

    Add files via upload

---
+++ Problems On Bitwise Operators/Program207.c
@@ -0,0 +1,39 @@
+/*
+
+Representation of Hexadecimal:-
+
+Decimal     Binary      Hexadecimal
+0           0000        0
+1           0001        1
+2           0010        2
+3           0011        3
+4           0100        4
+5           0110        5
+6           0110        6
+7           0111        7
+8           1000        8
+9           1001        9
+10          1010        a
+11          1011        b
+12          1100        c
+13          1101        d
+14          1110        e
+15          1111        f
+
+
+int no = 12         1   1   0   0
+
+In actual RAM its stored as: 
+1 nible = 4 bits
+Total 8 nibles = 32 bits
+
+0 0 0 0    0 0 0 0    0 0 0 0    0 0 0 0    0 0 0 0    0 0 0 0      0 0 0 0     1 1 0 0    
+
+=> 0          0         0          0          0          0            0           c  
+
+                                   =>     0x0000000b
+
+*/
+
+
+
