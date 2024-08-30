8d8dd431177fdfd62e26527d1a560ff0ff72b02a
Atharva Bhoite <105184536+78033@users.noreply.github.com>
2024-08-30 10:59:34 +0530

    Add files via upload

---
+++ File Handling/CopyContents280.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<unistd.h>      
+#include<fcntl.h>     
+#include<stdlib.h>
+#include<string.h>
+
+
+#define BUFFER_SIZE 1024        // macro
+
+int main(){
+
+    int fdRead = 0, fdWrite = 0;    
+    char FileName[30];
+    char Buffer[BUFFER_SIZE] = {'\0'};     // 1024: 1Kb
+    int iRet = 0;
+
+
+    // TO read file
+    printf("Enter the existing file name that you want to open: \n");
+    scanf("%s", FileName);
+
+    fdRead = open(FileName, O_RDWR);     
+
+    if(fdRead == -1){
+        printf("Unable to open file\n");
+        return 0;
+    }
+
+
+
+    // To create new file to write into it
+    printf("Enter the name of file that you want to create: \n");
+    scanf("%s", FileName);
+
+    fdWrite = creat(FileName, 0777);
+    
+    if(fdWrite == -1){
+        printf("Unable to create new file\n");
+        return 0;
+    }
+
+
+
+    // To copy the contents of read file into newly created file which is fdWrite
+    while((iRet = read(fdRead, Buffer, sizeof(Buffer))) != 0){     
+        write(fdWrite, Buffer, iRet);       // This will copy the contents of fdRead file into fdWrite file
+
+        memset(Buffer, 0, sizeof(Buffer));      // To clean the data (flush before using data)
+    }
+
+    close(fdRead);
+    close(fdWrite);
+
+    return 0;
+}
+
+
