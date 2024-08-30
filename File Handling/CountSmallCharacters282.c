4bd47106da5227d34f7d8021e713504b2f661512
Atharva Bhoite <105184536+78033@users.noreply.github.com>
2024-08-30 11:00:13 +0530

    Add files via upload

---
+++ File Handling/CountSmallCharacters282.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<unistd.h>      
+#include<fcntl.h>     
+#include<stdlib.h>
+#include<string.h>
+
+
+int main(){
+
+    int fd = 0;     // fd: file descriptor
+    char FileName[30];
+    char Buffer[100] = {'\0'};
+    int iRet = 0;
+    int iCnt = 0;
+
+    printf("Enter the file name that you want to open: \n");
+    scanf("%s", FileName);
+
+    fd = open(FileName, O_RDWR);     // O_APPEND : to avoid the overwriting the data    
+
+    if(fd == -1){
+        printf("Unable to open file\n");
+        return 0;
+    }
+
+    while((iRet = read(fd, Buffer, sizeof(Buffer))) != 0){      // read data from file fd, jevda buffer madhe basel tevda, sizeof(Buffer) = 100 so it will first read 100, then second time 100 and go's on
+        for(int i = 0; i < iRet; i++){
+            if(Buffer[i] > 'a' && Buffer[i] < 'z'){
+                iCnt++;
+            }
+        }
+    }
+
+    printf("Number of small characters are: %d\n", iCnt);
+
+    close(fd);
+
+    return 0;
+}
+
+
