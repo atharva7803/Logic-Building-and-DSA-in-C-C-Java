e2f62fa99a208790810a1c8c6e9584bfd39bca1f
Atharva Bhoite <105184536+78033@users.noreply.github.com>
2024-09-04 22:33:54 +0530

    Add files via upload

---
+++ File Handling/CountCharacter287.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<unistd.h>      
+#include<fcntl.h>     
+#include<stdlib.h>
+#include<string.h>
+
+
+int main(){
+
+    int fd = 0;     // fd: file descriptor
+    char FileName[30];
+    char Buffer[100] = {'\0'};
+    int iRet = 0;
+    int iCnt = 0;
+    char ch = ' ';
+
+    printf("Enter the file name that you want to open: \n");
+    scanf("%s", FileName);
+
+    fd = open(FileName, O_RDWR);     // O_APPEND : to avoid the overwriting the data    
+
+    if(fd == -1){
+        printf("Unable to open file\n");
+        return 0;
+    }
+
+    printf("Enter the character that you want to search: \n");
+    scanf(" %c", ch);
+
+    while((iRet = read(fd, Buffer, sizeof(Buffer))) != 0){      // read data from file fd, jevda buffer madhe basel tevda, sizeof(Buffer) = 100 so it will first read 100, then second time 100 and go's on
+        for(int i = 0; i < iRet; i++){
+            if(Buffer[i] == ch){
+                iCnt++;
+            }
+        }
+    }
+
+    printf("Number of occurance of character is: %d\n", iCnt);
+
+    close(fd);
+
+    return 0;
+}
+
+
