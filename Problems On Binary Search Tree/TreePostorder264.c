2314b362a95b87051ed5dae2778ffaabeed76ffc
Atharva Bhoite <105184536+atharva7803@users.noreply.github.com>
2024-09-18 23:07:20 +0530

    Add files via upload

---
+++ Problems On Binary Search Tree/TreePostorder264.c
@@ -0,0 +1,111 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node * lchild;       // left child
+    struct node * rchild;       // right child
+};
+
+typedef struct node Node;
+typedef struct node* PNode;
+typedef struct node** PPNode;
+
+void Insert(PPNode root, int no){
+    PNode newn = NULL;
+    PNode temp = NULL;
+
+    newn = (PNode)malloc(sizeof(Node));
+
+    newn -> data = no;
+    newn -> lchild = NULL;
+    newn -> rchild = NULL;
+
+    if(*root == NULL){      // Tree is empty
+        *root = newn;
+    }
+    else{                   //  Tree contains at least one node
+        temp = *root;
+        while(1){
+            if(no == temp -> data){
+                printf("Unable to insert as element is already present(Duplicate value)\n");
+                free(newn);
+                break;
+            }
+
+            else if(no > temp -> data){
+                if(temp -> rchild == NULL){
+                    temp -> rchild = newn;
+                    break;
+                }
+                temp = temp -> rchild;
+            }
+
+            else if(no < temp -> data){
+                if(temp -> lchild == NULL){
+                    temp -> lchild = newn;
+                    break;
+                }
+                temp = temp -> lchild;
+            }
+        }
+    }
+}
+
+
+
+void Inorder(PNode root){
+    if(root != NULL) {
+        Inorder(root->lchild);
+        printf("%d ", root->data);
+        Inorder(root->rchild);
+    }
+    
+}
+
+
+void Preorder(PNode root){
+    if(root != NULL) {
+        printf("%d ", root->data);
+        Inorder(root->lchild);
+        Inorder(root->rchild);
+    }
+}
+
+
+
+void Postorder(PNode root){
+    if(root != NULL) {
+        Inorder(root->lchild);
+        Inorder(root->rchild);
+        printf("%d ", root->data);
+    }
+}
+
+
+
+
+int main(){
+
+
+    PNode head = NULL;
+
+    Insert(&head, 21);
+    Insert(&head, 11);
+    Insert(&head, 51);
+
+
+    printf("PreOrder Traversal: \n");
+    Inorder(head);
+    printf("\n");
+
+    printf("PreOrder Traversal: \n");
+    Preorder(head);
+    printf("\n");
+
+    printf("PostOrder Traversal: \n");
+    Postorder(head);
+    printf("\n");
+
+    return 0;
+}
