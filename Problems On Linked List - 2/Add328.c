e8c632e15fcf091fc640ab3ff4fad84abb3e0383
Atharva Bhoite <105184536+atharva7803@users.noreply.github.com>
2024-10-02 22:49:15 +0530

    Add files via upload

---
+++ Problems On Linked List - 2/Add328.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE First, int No)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = No;
+    newn->next = NULL;
+
+    if(*First == NULL)
+    {
+        *First = newn;
+    }
+    else
+    {
+        newn->next = *First;
+        *First = newn;
+    }
+}   
+
+void Display(PNODE First)
+{
+    while(First != NULL)
+    {
+        printf("| %d |->",First->data);
+        First = First -> next;
+    }
+    printf("NULL\n");
+} 
+
+int Count(PNODE First)
+{
+    int iCnt = 0;
+
+    while(First != NULL)
+    {
+        iCnt++;
+        First = First -> next;
+    }
+    return iCnt;
+} 
+
+
+
+int Addition(PNODE First){
+    int iSum = 0;
+
+    while(First != NULL){
+        iSum = iSum + First->data;
+        First = First -> next;
+    }
+    return iSum;
+}
+
+
+
+int main()
+{
+    PNODE Head = NULL;
+    int iRet = 0;
+    
+    InsertFirst(&Head,42);
+    InsertFirst(&Head,10);
+    InsertFirst(&Head,50);
+    InsertFirst(&Head,21);
+    InsertFirst(&Head,10);
+    InsertFirst(&Head,21);
+    InsertFirst(&Head,11);
+    
+    Display(Head);
+
+    iRet = Addition(Head);
+    printf("Addition of elements is:%d\n",iRet);
+    
+    return 0;
+}
