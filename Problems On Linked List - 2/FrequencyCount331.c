a9f7fe3e4f6ffa5e1361d74c2520396379ed12d2
Atharva Bhoite <105184536+atharva7803@users.noreply.github.com>
2024-10-03 22:27:27 +0530

    Add files via upload

---
+++ Problems On Linked List - 2/FrequencyCount331.c
@@ -0,0 +1,131 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE First, int No)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = No;
+    newn->next = NULL;
+
+    if(*First == NULL)
+    {
+        *First = newn;
+    }
+    else
+    {
+        newn->next = *First;
+        *First = newn;
+    }
+}   
+
+void Display(PNODE First)
+{
+    while(First != NULL)
+    {
+        printf("| %d |->",First->data);
+        First = First -> next;
+    }
+    printf("NULL\n");
+} 
+
+int Count(PNODE First)
+{
+    int iCnt = 0;
+
+    while(First != NULL)
+    {
+        iCnt++;
+        First = First -> next;
+    }
+    return iCnt;
+} 
+
+
+
+int Addition(PNODE First){
+    int iSum = 0;
+
+    while(First != NULL){
+        iSum = iSum + First->data;
+        First = First -> next;
+    }
+    return iSum;
+}
+
+
+
+int EvenCount(PNODE First){
+    int iCnt = 0;
+
+    while(First != NULL){
+        if((First->data) % 2 == 0){
+            iCnt++;
+        }
+        First = First -> next;
+    }
+    return iCnt;
+}
+
+
+
+int OddCount(PNODE First){
+    int iCnt = 0;
+
+    while(First != NULL){
+        if((First->data) % 2 != 0){
+            iCnt++;
+        }
+        First = First -> next;
+    }
+    return iCnt;
+}
+
+
+
+int FreqCount(PNODE First, int no){
+    int iCnt = 0;
+    
+    while(First != NULL){
+        if((First->data) == no){
+            iCnt++;
+        }
+        First = First -> next;
+    }
+    return iCnt;
+}
+
+
+
+int main()
+{
+    PNODE Head = NULL;
+    int iRet = 0;
+    
+    InsertFirst(&Head,42);
+    InsertFirst(&Head,10);
+    InsertFirst(&Head,50);
+    InsertFirst(&Head,21);
+    InsertFirst(&Head,10);
+    InsertFirst(&Head,21);
+    InsertFirst(&Head,11);
+    
+    Display(Head);
+
+    iRet = FreqCount(Head, 21);
+    printf("Frequency of no is:%d\n",iRet);
+    
+    return 0;
+}
