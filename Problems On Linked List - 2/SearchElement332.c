35e6fd2d9e3ad404559bb2f5fe7d9d7e411573c7
Atharva Bhoite <105184536+atharva7803@users.noreply.github.com>
2024-10-04 22:20:10 +0530

    Add files via upload

---
+++ Problems On Linked List - 2/SearchElement332.c
@@ -0,0 +1,153 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE First, int No)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = No;
+    newn->next = NULL;
+
+    if(*First == NULL)
+    {
+        *First = newn;
+    }
+    else
+    {
+        newn->next = *First;
+        *First = newn;
+    }
+}   
+
+void Display(PNODE First)
+{
+    while(First != NULL)
+    {
+        printf("| %d |->",First->data);
+        First = First -> next;
+    }
+    printf("NULL\n");
+} 
+
+int Count(PNODE First)
+{
+    int iCnt = 0;
+
+    while(First != NULL)
+    {
+        iCnt++;
+        First = First -> next;
+    }
+    return iCnt;
+} 
+
+
+
+int Addition(PNODE First){
+    int iSum = 0;
+
+    while(First != NULL){
+        iSum = iSum + First->data;
+        First = First -> next;
+    }
+    return iSum;
+}
+
+
+
+int EvenCount(PNODE First){
+    int iCnt = 0;
+
+    while(First != NULL){
+        if((First->data) % 2 == 0){
+            iCnt++;
+        }
+        First = First -> next;
+    }
+    return iCnt;
+}
+
+
+
+int OddCount(PNODE First){
+    int iCnt = 0;
+
+    while(First != NULL){
+        if((First->data) % 2 != 0){
+            iCnt++;
+        }
+        First = First -> next;
+    }
+    return iCnt;
+}
+
+
+
+int FreqCount(PNODE First, int no){
+    int iCnt = 0;
+    
+    while(First != NULL){
+        if((First->data) == no){
+            iCnt++;
+        }
+        First = First -> next;
+    }
+    return iCnt;
+}
+
+
+
+bool SearchElement(PNODE First, int no){
+    bool bFlag = false;
+    
+    while(First != NULL){
+        if((First->data) == no){
+            bFlag = true;
+            break;
+        }
+        First = First -> next;
+    }
+    return bFlag;
+}
+
+
+
+int main()
+{
+    PNODE Head = NULL;
+    int iRet = 0;
+    bool bRet = false;
+    
+    InsertFirst(&Head,42);
+    InsertFirst(&Head,10);
+    InsertFirst(&Head,50);
+    InsertFirst(&Head,21);
+    InsertFirst(&Head,10);
+    InsertFirst(&Head,21);
+    InsertFirst(&Head,11);
+    
+    Display(Head);
+
+    bRet = SearchElement(Head, 50);
+    if(bRet == true){
+        printf("Element is present in the linked list\n");
+    }
+    else{
+        printf("Element is not present in the linked list\n");
+    }
+    
+    return 0;
+}
