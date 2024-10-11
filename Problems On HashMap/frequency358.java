9c3c9e552ca45b73c7d9482709efd970d2e5f9dd
Atharva Bhoite <105184536+atharva7803@users.noreply.github.com>
2024-10-11 22:29:55 +0530

    Add files via upload

---
+++ Problems On HashMap/frequency358.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class frequency358{
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter string: ");
+        String str = sc.nextLine();
+
+        char arr[] = str.toCharArray();
+
+        for(char ch : arr){
+            System.out.println(ch);
+        }
+
+
+    }
+}
