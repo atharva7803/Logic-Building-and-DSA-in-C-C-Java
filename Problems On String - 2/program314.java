12eef7d22e3252a65bb9bdccd6facedd63c1f3b8
Atharva Bhoite <105184536+atharva7803@users.noreply.github.com>
2024-10-15 19:58:38 +0530

    Add files via upload

---
+++ Problems On String - 2/program314.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class program314 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); 
+        
+        System.out.println("Enter string: ");
+        String str = sc.nextLine();
+        
+        System.out.println("Entered string is: "+ str);
+
+
+    }
+}
